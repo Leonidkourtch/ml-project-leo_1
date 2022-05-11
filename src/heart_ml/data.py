839d56b27e384f37a59e4d19b3db9613c3940994
Leonidkourtch <leonid.kurch@gmail.com>
2022-05-11 18:08:20 +0300

    27

--- src/heart_ml/data.py
+++ src/heart_ml/data.py
@@ -26,3 +26,32 @@ def get_dataset(
         features, target, test_size=test_split_ratio, random_state=random_state
     )
     return features_train, features_val, target_train, target_val
+
+# configure the cross-validation procedure
+cv_outer = KFold(n_splits=10, shuffle=True, random_state=1)
+# enumerate splits
+outer_results = list()
+for train_ix, test_ix in cv_outer.split(get_dataset()):
+    # split data
+	X_train, X_test = get_dataset()[train_ix, :], get_dataset()[test_ix, :]
+	y_train, y_test = get_dataset()[train_ix], get_dataset()[test_ix]
+	# configure the cross-validation procedure
+	cv_inner = KFold(n_splits=3, shuffle=True, random_state=1)
+	# define the model
+	model = RandomForestClassifier(random_state=1)
+	# define search space
+	space = dict()
+	space['n_estimators'] = [10, 100, 500]
+	space['max_features'] = [2, 4, 6]
+	# define search
+	search = GridSearchCV(model, space, scoring='accuracy', cv=cv_inner, refit=True)
+	# execute search
+	result = search.fit(X_train, y_train)
+	# get the best performing model fit on the whole training set
+	best_model = result.best_estimator_
+	# evaluate model on the hold out dataset
+	yhat = best_model.predict(X_test)
+	# evaluate the model
+	acc = accuracy_score(y_test, yhat)
+	# store the result
+	outer_results.append(acc)
